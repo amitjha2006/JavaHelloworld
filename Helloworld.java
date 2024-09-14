dc5dede6938d16134806c7b39cf7a9775caae759
Amitjha2006 <amitjha2006@gmail.com>
2024-09-14 12:43:03 +0530

    my first commit

---
+++ Helloworld.java
@@ -0,0 +1,17 @@
+package myjavafirstprogram;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1;1<=10;i++) {
+			System.out.println("Hello World...");
+			if (i == 10) {
+		        break; // Exits the loop when i reaches 10
+		    }
+			
+		}
+
+	}
+
+}
